c51500ee1c435e94a6c6ec5f0396f1fc5891b337
Sairahcaz <zacharias.creutznacher@gmail.com>
2023-04-08 22:27:33 +0200

    pest laravel 9 compatibility

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,9 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
-});
-
 it('covers all cases', function () {
+    //TODO
     // Write tests for all kind of scopes
     // Create a db with fixed date values
     // and test with: Carbon::setTestNow('2023-01-01');
-})->todo();
+    expect(true)->toBeTrue();
+});
